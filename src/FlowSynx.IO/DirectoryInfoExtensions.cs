e607e4ff1cd0e64a1c83c8d6ceba0427c772f53e
ziagham <amin.ziagham@gmail.com>
2023-12-19 21:47:40 +0100

    Add extension for directoryinfo for finding files and directories.

---
+++ src/FlowSynx.IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace FlowSynx.IO;
+
+public static class DirectoryInfoExtensions
+{
+    public static IEnumerable<FileInfo> FindFiles(this DirectoryInfo directoryInfo, string filter = "*", bool recursive = false)
+    {
+        IEnumerator<FileInfo> enumerator;
+        try
+        {
+            enumerator = directoryInfo
+                .EnumerateFiles(filter, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                .GetEnumerator();
+        }
+        catch (UnauthorizedAccessException) { yield break; }
+        while (true)
+        {
+            try { if (!enumerator.MoveNext()) break; }
+            catch (UnauthorizedAccessException) { continue; }
+            yield return enumerator.Current;
+        }
+    }
+
+    public static IEnumerable<DirectoryInfo> FindDirectories(this DirectoryInfo directoryInfo, string filter = "*", bool recursive = false)
+    {
+        IEnumerator<DirectoryInfo> enumerator;
+        try
+        {
+            enumerator = directoryInfo
+                .EnumerateDirectories(filter, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                .GetEnumerator();
+        }
+        catch (UnauthorizedAccessException) { yield break; }
+        while (true)
+        {
+            try { if (!enumerator.MoveNext()) break; }
+            catch (UnauthorizedAccessException) { continue; }
+            yield return enumerator.Current;
+        }
+    }
+}
---
+++ src/FlowSynx.IO/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FlowSynx.IO.FileSystem;
+using FlowSynx.IO.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowSynx.IO;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddFlowSynxSerialization(this IServiceCollection services)
+    {
+        services
+            .AddTransient<ISerializer, JsonSerializer>()
+            .AddTransient<IDeserializer, JsonDeserializer>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddFlowSynxFileSystem(this IServiceCollection services)
+    {
+        services
+            .AddTransient<IFileReader, FileReader>()
+            .AddTransient<IFileWriter, FileWriter>();
+
+        return services;
+    }
+}
