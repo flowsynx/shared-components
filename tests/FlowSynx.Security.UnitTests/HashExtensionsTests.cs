b3bfc5ae9c95a004c54503b0a44f6e4e344fba44
ziagham <amin.ziagham@gmail.com>
2024-01-07 15:50:36 +0100

    Move the unit tests projects to the proper path, as well as add new unit test project for the FlowSynx.Security project.

---
+++ tests/FlowSynx.Security.UnitTests/HashExtensionsTests.cs
@@ -0,0 +1,13 @@
+namespace FlowSynx.Security.UnitTests;
+
+public class HashExtensionsTests
+{
+    [Theory]
+    [InlineData("TestFile", "E1F53BF8B56F5DF417E0039934F5B7C2")]
+    [InlineData("FlowSynx.exe", "7F3A8BD187E6014E1D5269CB22753BDB")]
+    [InlineData("Program Files", "0D4F03E079D7F47617746E2CA64C1469")]
+    public void GivenAString_WhenCreateMd5IsCalled_ThenMd5IsReturned(string input, string expected)
+    {
+        Assert.Equal(expected, input.CreateMd5());
+    }
+}
