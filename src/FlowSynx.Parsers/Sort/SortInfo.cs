b32033cd13d28ddfeb1e3b6331daa758ef8a4c30
ziagham <amin.ziagham@gmail.com>
2024-01-08 23:08:01 +0100

    Add IEquatable interface as well its implementation in order to check the equality of two SortInfo instances.

--- src/FlowSynx.Parsers/Sort/SortInfo.cs
+++ src/FlowSynx.Parsers/Sort/SortInfo.cs
@@ -1,7 +1,21 @@
 ﻿namespace FlowSynx.Parsers.Sort;
 
-public class SortInfo
+public class SortInfo: IEquatable<SortInfo>
 {
     public required string Name { get; init; }
     public required SortDirection Direction { get; init; }
+
+    public bool Equals(SortInfo? other)
+    {
+        if (other == null)
+            return false;
+        
+        if (ReferenceEquals(this, other))
+            return true;
+        
+        if (Name != other.Name) return false;
+        if (Direction != other.Direction) return false;
+        
+        return true;
+    }
 }
