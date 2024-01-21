c0f3befb31ed8ec7c65ac0817369ca382eb3797c
ziagham <amin.ziagham@gmail.com>
2024-01-21 16:50:30 +0100

    Refactoring the code in order to read default http port

--- src/FlowSynx.Environment/DefaultEndpoint.cs
+++ src/FlowSynx.Environment/DefaultEndpoint.cs
@@ -5,24 +5,12 @@ namespace FlowSynx.Environment;
 public class DefaultEndpoint : IEndpoint
 {
     private readonly ILogger<DefaultEndpoint> _logger;
-    private readonly IEnvironmentManager _environmentManager;
 
-    public DefaultEndpoint(ILogger<DefaultEndpoint> logger,IEnvironmentManager environmentManager)
+    public DefaultEndpoint(ILogger<DefaultEndpoint> logger)
     {
         _logger = logger;
-        _environmentManager = environmentManager;
     }
 
-    public int GetDefaultHttpPort()
-    {
-        var flowSyncPort = _environmentManager.Get(EnvironmentVariables.FlowsynxHttpPort);
-        var _ = int.TryParse(flowSyncPort, out var result);
-        return result > 0 ? result : EnvironmentVariables.FlowsynxDefaultPort;
-    }
-
-    public string GetDefaultHttpEndpoint()
-    {
-        var port = GetDefaultHttpPort();
-        return $"http://127.0.0.1:{port}";
-    }
+    public int GetDefaultHttpPort() => EnvironmentVariables.FlowsynxDefaultPort;
+    public string GetDefaultHttpEndpoint() => $"http://localhost:{GetDefaultHttpPort()}";
 }
