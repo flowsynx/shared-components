cc4125a853a4d3df66f7a907355068c8ace2c434
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-03-19 08:41:20 +0100

    Adding Md5 property to the StorageRead model

--- src/FlowSynx.Plugin.Storage/StorageRead.cs
+++ src/FlowSynx.Plugin.Storage/StorageRead.cs
@@ -5,4 +5,5 @@ public class StorageRead
     public required StorageStream Stream { get; set; }
     public string? Extension { get; set; }
     public string? MimeType { get; set; }
+    public string? Md5 { get; set; }
 }
