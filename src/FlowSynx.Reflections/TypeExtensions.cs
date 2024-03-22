2ffdb1c17e8d585236b764fffcefd6fd10fb7417
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-03-22 21:30:12 +0100

    add DictionaryToObject extension for mapping a dictionary to an object

--- src/FlowSynx.Reflections/TypeExtensions.cs
+++ src/FlowSynx.Reflections/TypeExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace FlowSynx.Reflections;
+﻿using System.Reflection;
+
+namespace FlowSynx.Reflections;
 
 public static class TypeExtensions
 {
@@ -30,5 +32,23 @@ public static class TypeExtensions
     public static bool IsAssignable(this Type concretion, Type abstraction)
     {
         return abstraction.IsAssignableFrom(concretion);
+    }
+
+    public static T DictionaryToObject<T>(this Dictionary<string, object?>? source,
+        BindingFlags bindingAttr = BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase) 
+        where T : class, new()
+    {
+        var newInstance = new T();
+        if (source is null) return newInstance;
+
+        var someObjectType = newInstance.GetType();
+        foreach (var item in source)
+        {
+            var property = someObjectType.Property(item.Key, bindingAttr);
+            if (property != null)
+                property.SetValue(newInstance, item.Value, null);
+        }
+
+        return newInstance;
     }
 }
