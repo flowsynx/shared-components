07d4b16717681d132b0cffdc4458c0cfe6642133
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-04-07 12:37:59 +0200

    add CompressionException to handle Exceptions regarding compressions

---
+++ src/FlowSynx.IO/Exceptions/CompressionException.cs
@@ -0,0 +1,9 @@
+﻿using FlowSynx.Abstractions.Exceptions;
+
+namespace FlowSynx.IO.Exceptions;
+
+public class CompressionException : FlowSynxException
+{
+    public CompressionException(string message) : base(message) { }
+    public CompressionException(string message, Exception inner) : base(message, inner) { }
+}
