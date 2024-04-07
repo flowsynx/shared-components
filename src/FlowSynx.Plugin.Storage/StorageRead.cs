a504fb679ace06b4c6e163c887e89c77726cda05
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-04-07 12:35:41 +0200

    rename MimeType property name to the ContentType

--- src/FlowSynx.Plugin.Storage/StorageRead.cs
+++ src/FlowSynx.Plugin.Storage/StorageRead.cs
@@ -4,6 +4,6 @@ public class StorageRead
 {
     public required StorageStream Stream { get; set; }
     public string? Extension { get; set; }
-    public string? MimeType { get; set; }
+    public string? ContentType { get; set; }
     public string? Md5 { get; set; }
 }
