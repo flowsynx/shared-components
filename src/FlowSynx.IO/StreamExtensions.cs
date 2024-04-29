dab3567ba245c9ce4c78cd58336c168f34ddade0
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-04-29 06:05:44 +0200

    add new extension for stream in order to WriteTo stream to a file.

--- src/FlowSynx.IO/StreamExtensions.cs
+++ src/FlowSynx.IO/StreamExtensions.cs
@@ -13,4 +13,12 @@ public static class StreamExtensions
 
         return Convert.ToBase64String(bytes);
     }
+
+    public static void WriteTo(this Stream stream, string path)
+    {
+        using (var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            stream.CopyTo(fileStream);
+        }
+    }
 }
