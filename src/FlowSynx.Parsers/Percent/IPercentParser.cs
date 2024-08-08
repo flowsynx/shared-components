864d8f3ccf417114e1d6e7f6d6e185a526185f09
ziagham <amin.ziagham@outlook.com>
2024-08-08 23:13:55 +0200

    Delete FlowSynx.Plugin.Abstractions project from Parser project and add PredicateBuilder to it #16

--- src/FlowSynx.Parsers/Percent/IPercentParser.cs
+++ src/FlowSynx.Parsers/Percent/IPercentParser.cs
@@ -1,7 +1,4 @@
-﻿using FlowSynx.Parsers;
-using FlowSynx.Plugin.Abstractions;
-
-namespace FlowSynx.Parsers.Percent;
+﻿namespace FlowSynx.Parsers.Percent;
 
 public interface IPercentParser : IParser
 {
