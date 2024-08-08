a247d6315355e96d9f467e9a026601e22fc919f5
ziagham <amin.ziagham@outlook.com>
2024-08-08 23:09:46 +0200

    Move attributes classes from FlowSynx.Plugin.Abstractions to FlowSynx.Abstractions #14

--- src/FlowSynx.Abstractions/Attributes/RequiredMemberAttribute.cs
+++ src/FlowSynx.Abstractions/Attributes/RequiredMemberAttribute.cs
@@ -1,4 +1,4 @@
-﻿namespace FlowSynx.Plugin.Abstractions;
+﻿namespace FlowSynx.Abstractions.Attributes;
 
 [AttributeUsage(AttributeTargets.Property)]
 public class RequiredMemberAttribute : Attribute
