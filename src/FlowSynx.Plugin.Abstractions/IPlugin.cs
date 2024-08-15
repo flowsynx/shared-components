1b649b8db579131c953fd3c2f55cef7ed7905430
ziagham <amin.ziagham@outlook.com>
2024-08-15 17:00:43 +0200

    Integrate all plugins in one Interface and make it loosely coupled methods #27

--- src/FlowSynx.Plugin.Abstractions/IPlugin.cs
+++ src/FlowSynx.Plugin.Abstractions/IPlugin.cs
@@ -17,4 +17,24 @@ public interface IPlugin
     Dictionary<string, string?>? Specifications { get; set; }
 
     Type SpecificationsType { get; }
+
+    Task<object> About(string entity, CancellationToken cancellationToken = default);
+
+    Task<object> CreateAsync(string entity, Dictionary<string, object>? options,
+        CancellationToken cancellationToken = default);
+
+    Task<object> WriteAsync(string entity, Dictionary<string, object>? options, object dataOptions,
+        CancellationToken cancellationToken = default);
+
+    Task<object> ReadAsync(string entity, Dictionary<string, object>? options,
+        CancellationToken cancellationToken = default);
+
+    Task<object> UpdateAsync(string entity, Dictionary<string, object>? options,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<object>> DeleteAsync(string entity, Dictionary<string, object>? options,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<object>> ListAsync(string entity, Dictionary<string, object>? options,
+        CancellationToken cancellationToken = default);
 }
