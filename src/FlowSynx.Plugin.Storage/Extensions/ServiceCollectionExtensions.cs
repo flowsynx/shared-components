06402375c4cf60290721b63562530fd7411f8218
ziagham <amin.ziagham@outlook.com>
2024-08-21 23:07:43 +0200

    Add ServiceCollectionExtensions to the FlowSynx.Plugin.Storage for adding StorageFilter service #33

---
+++ src/FlowSynx.Plugin.Storage/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowSynx.Plugin.Storage.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddStorageFilter(this IServiceCollection services)
+    {
+        services.AddScoped<IStorageFilter, StorageFilter>();
+
+        return services;
+    }
+}
