20cec6b8404501e58a1c27735c272c759c9c512d
ziagham <amin.ziagham@outlook.com>
2024-08-23 12:57:34 +0200

    Change Access Modifier of StorageList for some proprties #36

--- src/FlowSynx.Plugin.Storage/StorageList.cs
+++ src/FlowSynx.Plugin.Storage/StorageList.cs
@@ -2,10 +2,10 @@
 
 public class StorageList
 {
-    public string? Id { get; set; }
+    public required string Id { get; set; }
     public string? Kind { get; set; }
-    public string? Name { get; set; } = string.Empty;
-    public string? Path { get; set; } = string.Empty;
+    public required string Name { get; set; } = string.Empty;
+    public required string Path { get; set; } = string.Empty;
     public string? Size { get; set; }
     public string? ContentType { get; set; }
     public DateTimeOffset? ModifiedTime { get; set; }
