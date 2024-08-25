a074370d9b0a846098bc892e5ac7682fb54adb19
ziagham <amin.ziagham@outlook.com>
2024-08-25 15:37:23 +0200

    Change StorageUsage properties from long to string #40

--- src/FlowSynx.Plugin.Storage/StorageUsage.cs
+++ src/FlowSynx.Plugin.Storage/StorageUsage.cs
@@ -2,7 +2,7 @@
 
 public class StorageUsage
 {
-    public long Total { get; set; }
-    public long Free { get; set; }
-    public long Used { get; set; }
+    public string? Total { get; set; }
+    public string? Free { get; set; }
+    public string? Used { get; set; }
 }
