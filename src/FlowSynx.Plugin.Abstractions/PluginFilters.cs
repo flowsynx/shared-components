92fc11e802487f475d7e86e214d776618e3bba4f
ziagham <amin.ziagham@outlook.com>
2024-09-02 21:59:53 +0200

    Add ChangeProperty to the PluginFilters to makes it able to change specific property based on its name #49

--- src/FlowSynx.Plugin.Abstractions/PluginFilters.cs
+++ src/FlowSynx.Plugin.Abstractions/PluginFilters.cs
@@ -2,5 +2,11 @@
 
 public class PluginFilters : Dictionary<string, object?>
 {
-
+    public void ChangeProperty(string propertyName, object propertyValue)
+    {
+        if (this.ContainsKey(propertyName))
+        {
+            this[propertyName] = propertyValue;
+        }
+    }
 }
