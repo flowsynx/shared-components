42ed451b922eea639b544b118b557e9d6482c14b
ziagham <amin.ziagham@outlook.com>
2024-09-02 22:02:44 +0200

    Add contractors to the TransmissionData #50

--- src/FlowSynx.Plugin.Abstractions/TransmissionData.cs
+++ src/FlowSynx.Plugin.Abstractions/TransmissionData.cs
@@ -2,5 +2,25 @@
 
 public class TransmissionData
 {
-    public required object Content { get; set; }
+    public string Key { get; set; }
+    public object? Content { get; set; }
+    public string? ContentType { get; set; }
+
+    public TransmissionData(string key)
+    {
+        Key = key;
+    }
+
+    public TransmissionData(string key, object content)
+    {
+        Key = key;
+        Content = content;
+    }
+
+    public TransmissionData(string key, object content, string? contentType)
+    {
+        Key = key;
+        Content = content;
+        ContentType = contentType;
+    }
 }
