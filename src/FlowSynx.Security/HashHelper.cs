04a52a641d3c312acaddef23e7c65d1f1f3fa56b
ziagham <amin.ziagham@outlook.com>
2024-09-10 08:31:35 +0200

    Add new function for hashing md5 for an object #60

--- src/FlowSynx.Security/HashHelper.cs
+++ src/FlowSynx.Security/HashHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
 
 namespace FlowSynx.Security;
 
@@ -48,6 +50,23 @@ public class HashHelper
                 return string.Empty;
             }
         }
+
+        public static string GetHash(object obj)
+        {
+            try
+            {
+                using var hasher = MD5.Create();
+                using var stream = new MemoryStream();
+                var serializer = new DataContractSerializer(obj.GetType());
+                serializer.WriteObject(stream, obj);
+                var hashBytes = hasher.ComputeHash(stream.ToArray());
+                return Convert.ToHexString(hashBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
     }
 
     public class Sha256
@@ -87,6 +106,23 @@ public class HashHelper
             {
                 using var hasher = SHA256.Create();
                 var hashBytes = hasher.ComputeHash(stream);
+                return Convert.ToHexString(hashBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static string GetHash(object obj)
+        {
+            try
+            {
+                using var hasher = SHA256.Create();
+                using var stream = new MemoryStream();
+                var serializer = new DataContractSerializer(obj.GetType());
+                serializer.WriteObject(stream, obj);
+                var hashBytes = hasher.ComputeHash(stream.ToArray());
                 return Convert.ToHexString(hashBytes);
             }
             catch
