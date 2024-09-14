ceaae3d1865f2fa45550796acda691d4c4d4d70a
ziagham <amin.ziagham@outlook.com>
2024-09-14 23:21:34 +0200

    Add DataFilter to the FlowSynx.Plugin.Abstractions #64

---
+++ src/FlowSynx.Plugin.Abstractions/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FlowSynx.Plugin.Abstractions.Filter;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowSynx.Plugin.Abstractions.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatFilter(this IServiceCollection services)
+    {
+        services.AddScoped<IDataFilter, DataFilter>();
+        return services;
+    }
+}
---
+++ src/FlowSynx.Plugin.Abstractions/Filter/IDataFilter.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace FlowSynx.Plugin.Abstractions.Filter;
+
+public interface IDataFilter
+{
+    DataTable Filter(DataTable dataTable, DataFilterOptions? dataFilterOptions);
+}
