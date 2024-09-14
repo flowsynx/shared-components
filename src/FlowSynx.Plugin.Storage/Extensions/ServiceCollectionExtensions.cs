acc535379a801db560e9582676b506b1c98bd973
ziagham <amin.ziagham@outlook.com>
2024-09-14 23:27:38 +0200

    Add DataTable Extension class to the FlowSynx.Plugin.Storage project #67

--- src/FlowSynx.Plugin.Storage/Extensions/ServiceCollectionExtensions.cs
+++ src/FlowSynx.Plugin.Storage/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,59 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Data;
+using System.Reflection;
 
 namespace FlowSynx.Plugin.Storage.Extensions;
 
-public static class ServiceCollectionExtensions
+public static class DataTableExtensions
 {
-    public static IServiceCollection AddStorageFilter(this IServiceCollection services)
+    public static DataTable ToDataTable<T>(this List<T> items)
     {
-        services.AddScoped<IStorageFilter, StorageFilter>();
+        DataTable dataTable = new DataTable(typeof(T).Name);
+        var Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (PropertyInfo prop in Props)
+        {
+            var propType = prop.PropertyType;
+            if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                propType = Nullable.GetUnderlyingType(prop.PropertyType);
+            }
 
-        return services;
+            dataTable.Columns.Add(prop.Name, propType);
+        }
+
+        foreach (T item in items)
+        {
+            var values = new object?[Props.Length];
+            for (int i = 0; i < Props.Length; i++)
+            {
+                values[i] = Props[i].GetValue(item);
+            }
+            dataTable.Rows.Add(values);
+        }
+        return dataTable;
+    }
+
+    public static List<object> CreateListFromTable(this DataTable dataTable)
+    {
+        var result = new List<object>();
+        var colCount = dataTable.Columns.Count;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            dynamic objExpando = new System.Dynamic.ExpandoObject();
+            var obj = objExpando as IDictionary<string, object>;
+
+            for (var i = 0; i < colCount; i++)
+            {
+                var key = dr.Table.Columns[i].ColumnName.ToString();
+                var val = dr[key];
+
+                if (obj != null)
+                    obj[key] = val;
+            }
+
+            if (obj != null)
+                result.Add(obj);
+        }
+
+        return result;
     }
 }
