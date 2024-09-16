f0d0ea5a5b1c0cf1b57b39667873342124b37ad9
ziagham <amin.ziagham@outlook.com>
2024-09-16 17:53:45 +0200

    Add Fields property to the ListOptions in order to show only selected fields in result #77

--- src/FlowSynx.Plugin.Storage/Options/ListOptions.cs
+++ src/FlowSynx.Plugin.Storage/Options/ListOptions.cs
@@ -2,12 +2,11 @@
 
 public class ListOptions
 {
+    public string? Fields { get; set; }
     public string? Filter { get; set; }
     public bool CaseSensitive { get; set; } = false;
-    public bool? Full { get; set; } = true;
     public bool Recurse { get; set; } = false;
     public string? Sort { get; set; }
     public string? Limit { get; set; }
     public bool? IncludeMetadata { get; set; } = false;
-    public bool? Hashing { get; set; } = false;
 }
