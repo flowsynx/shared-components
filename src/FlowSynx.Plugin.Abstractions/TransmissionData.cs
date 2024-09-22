0945625a1b85f4023682194ae0c6126db19bbcd0
ziagham <amin.ziagham@outlook.com>
2024-09-23 00:08:57 +0200

    Refactoring TransmissionData in order to support Content stream #84

--- src/FlowSynx.Plugin.Abstractions/TransmissionData.cs
+++ src/FlowSynx.Plugin.Abstractions/TransmissionData.cs
@@ -2,25 +2,16 @@
 
 public class TransmissionData
 {
-    public string Key { get; set; }
-    public object? Content { get; set; }
+    public required PluginNamespace Namespace { get; set; }
+    public required string Type { get; set; }
+    public required IEnumerable<string> Columns { get; set; }
+    public required IEnumerable<TransmissionDataRow> Rows { get; set; }
+}
+
+public class TransmissionDataRow
+{
+    public string? Key { get; set; }
     public string? ContentType { get; set; }
-
-    public TransmissionData(string key)
-    {
-        Key = key;
-    }
-
-    public TransmissionData(string key, object content)
-    {
-        Key = key;
-        Content = content;
-    }
-
-    public TransmissionData(string key, object content, string? contentType)
-    {
-        Key = key;
-        Content = content;
-        ContentType = contentType;
-    }
+    public Stream? Content { get; set; }
+    public object[]? Data { get; set; }
 }
