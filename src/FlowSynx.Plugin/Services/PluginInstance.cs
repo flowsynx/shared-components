3bcdf8dceb197dbc5e7f36f608bba5510dff20ab
ziagham <amin.ziagham@outlook.com>
2024-10-01 23:52:27 +0200

    Refactoring PluginInstanceParser to support multiple plugins types for stream plugins #183

--- src/FlowSynx.Plugin/Services/PluginInstance.cs
+++ src/FlowSynx.Plugin/Services/PluginInstance.cs
@@ -5,21 +5,35 @@ namespace FlowSynx.Plugin.Services;
 
 public class PluginInstance
 {
-    public PluginInstance(PluginBase plugin, string entity, PluginSpecifications? specifications)
+    public PluginInstance(PluginBase primaryPlugin, string entity, PluginSpecifications? specifications)
     {
-        EnsureArg.IsNotNull(plugin, nameof(plugin));
-        Plugin = plugin;
+        EnsureArg.IsNotNull(primaryPlugin, nameof(primaryPlugin));
+        PrimaryPlugin = primaryPlugin;
+        SecondaryPlugin = null;
         Entity = entity;
         Specifications = specifications;
-        plugin.Specifications = specifications;
+        primaryPlugin.Specifications = specifications;
+    }
+
+    public PluginInstance(PluginBase primaryPlugin, PluginBase secondaryPlugin, string entity, PluginSpecifications? specifications)
+    {
+        EnsureArg.IsNotNull(primaryPlugin, nameof(primaryPlugin));
+        PrimaryPlugin = primaryPlugin;
+        SecondaryPlugin = secondaryPlugin;
+        Entity = entity;
+        Specifications = specifications;
+        primaryPlugin.Specifications = specifications;
     }
 
     public string Entity { get; set; }
-    public PluginBase Plugin { get; }
+    public PluginBase PrimaryPlugin { get; }
+    public PluginBase? SecondaryPlugin { get; }
     public PluginSpecifications? Specifications { get; }
 
     public Task Initialize()
     {
-        return Plugin.Initialize();
+        PrimaryPlugin.Initialize();
+        SecondaryPlugin?.Initialize();
+        return Task.CompletedTask;
     }
 }
