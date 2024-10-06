61f99db9569b96e99bd934076b785dd927e46b06
ziagham <amin.ziagham@outlook.com>
2024-10-06 23:05:46 +0200

    Delete unused classes from FlowSynx.Plugin.Storage project #101

--- src/FlowSynx.Plugin.Storage/StorageRead.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlowSynx.Plugin.Storage;
-
-public class StorageRead
-{
-    public required byte[] Content { get; set; }
-    public string? ContentType { get; set; }
-    public string? Extension { get; set; }
-    public string? Md5 { get; set; }
-}
--- src/FlowSynx.Plugin.Storage/StorageUsage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FlowSynx.Plugin.Storage;
-
-public class StorageUsage
-{
-    public string? Total { get; set; }
-    public string? Free { get; set; }
-    public string? Used { get; set; }
-}
