8440ad941323d3e2e4aed60e49981abb47d7f57a
ziagham <amin.ziagham@outlook.com>
2024-10-06 23:01:48 +0200

    Delete IPluginService #99

--- src/FlowSynx.Plugin/Extensions/ServiceCollectionExtensions.cs
+++ src/FlowSynx.Plugin/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using FlowSynx.Plugin.Manager;
-using FlowSynx.Plugin.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FlowSynx.Plugin.Extensions;
@@ -9,13 +8,6 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddPluginManager(this IServiceCollection services)
     {
         services.AddScoped<IPluginsManager, PluginsManager>();
-        return services;
-    }
-
-    public static IServiceCollection AddPluginService(this IServiceCollection services)
-    {
-        services.AddScoped<IPluginService, PluginService>();
-
         return services;
     }
 }
