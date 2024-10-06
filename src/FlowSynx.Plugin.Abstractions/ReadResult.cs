adce5545e3c1b36598cdf4da4aa0687247c034ec
ziagham <amin.ziagham@outlook.com>
2024-10-06 23:03:21 +0200

    Add ReadResult for using in PluginBase readAsync method #100

---
+++ src/FlowSynx.Plugin.Abstractions/ReadResult.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowSynx.Plugin.Abstractions;
+
+public class ReadResult
+{
+    public required byte[] Content { get; set; }
+    public long Length => Content?.Length ?? 0;
+    public string? ContentHash { get; set; }
+}
