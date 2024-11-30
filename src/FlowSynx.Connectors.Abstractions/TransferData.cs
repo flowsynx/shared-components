ee3053b36816ff08da52d0d5139346965e9f0401
ziagham <amin.ziagham@outlook.com>
2024-11-30 22:45:27 +0100

    Change Columns type from list of string to the list of Columns in TransferData #136

--- src/FlowSynx.Connectors.Abstractions/TransferData.cs
+++ src/FlowSynx.Connectors.Abstractions/TransferData.cs
@@ -6,8 +6,14 @@ public class TransferData
     public required string ConnectorType { get; set; }
     public string? ContentType { get; set; }
     public string? Content { get; set; }
-    public required IEnumerable<string> Columns { get; set; }
+    public required IEnumerable<TransferDataColumn> Columns { get; set; }
     public required IEnumerable<TransferDataRow> Rows { get; set; }
+}
+
+public class TransferDataColumn
+{
+    public required string Name { get; set; }
+    public Type? DataType { get; set; } = typeof(string);
 }
 
 public class TransferDataRow
