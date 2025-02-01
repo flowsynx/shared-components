58343f2194e7d23a38e3ea61f5be9224bd48d5f7
ziagham <amin.ziagham@outlook.com>
2025-02-01 22:57:44 +0100

    Add ConnectorMetadata attribute to keep some metadata information about connector #144

---
+++ src/FlowSynx.Connectors.Abstractions/ConnectorMetadataAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowSynx.Connectors.Abstractions;
+
+public class ConnectorMetadataAttribute: Attribute
+{
+    public LinkType LinkType { get; }
+
+    public ConnectorMetadataAttribute(LinkType linkType)
+    {
+        LinkType = linkType;
+    }
+}
---
+++ src/FlowSynx.Connectors.Abstractions/LinkType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowSynx.Connectors.Abstractions;
+
+public enum LinkType
+{
+    Input = 0,
+    Output,
+    InputOutput,
+}
